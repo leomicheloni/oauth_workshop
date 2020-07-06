d07e23438a64ab0f076f78b2224c9c4d63077a5a
Leonardo Micheloni <leomicheloni@hotmail.com>
2020-07-06 15:29:36 +0200

    Client Credentials implementation

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,66 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            TokenResponse tokenResponse = await RetrieveToken();
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:44321/weatherforecast");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+
+        private static async Task<TokenResponse> RetrieveToken()
+        {
+            TokenResponse tokenResponse;
+
+            // discover endpoints from metadata
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                throw new Exception();
+            }
+
+            // request token
+            tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client.credentials",
+                ClientSecret = "client.secret",
+                Scope = "API1"
+            });
+
+            return tokenResponse;
+        }
+    }
+}
--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,4 +1,5 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityModel;
+using IdentityServer4.Models;
 using System.Collections.Generic;
 
 namespace IdentityServer
@@ -8,12 +9,19 @@ namespace IdentityServer
         public static IEnumerable<ApiResource> Apis =>
             new List<ApiResource>
             {
-
+                new ApiResource("API1")
             };
 
         public static IEnumerable<Client> Clients =>
             new List<Client>
             {
+                new Client
+                {
+                    ClientId = "client.credentials",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = new [] { new Secret ("client.secret".ToSha256()) },
+                    AllowedScopes = new [] { "API1" }
+                }
             };
     }
 }
